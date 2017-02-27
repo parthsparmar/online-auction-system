8638a8605bcab1a98428db9d7829402a3b710b82
Parth <parthsparmar@github.com>
2017-02-27 23:22:48 +0530

    First commit

---
+++ homepage.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Online Auction</title>
+<link rel="stylesheet" type="text/css" href="defaultstyle.css">
+</head>
+
+<body>
+<header class="main-header">
+	<div class="logo">
+		<img src="logo.png" width="140px" height="28px">	
+	</div>	
+
+	<!-- HTML for SEARCH BAR -->
+	<div class="main-header-searchbar">
+		<form id="header_search_bar_form" method="get" action="http://www.google.com">
+			<input type="text" class="search-text" name="searchtext" size="100" maxlength="120">
+			<input type="submit" value="search" class="searchbtn">
+		</form>
+	</div>
+	
+</header>
+
+
+</body>
+</html>
